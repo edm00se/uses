4f891100813641eb7705c3334da380b9bd3e09e5
Eric McCormick <edm00se@users.noreply.github.com>
2022-07-15 21:46:37 +0000

    updates rollup and plugins following svelte upgrade

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -16,6 +17,7 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
+		css({ output: 'bundle.css' }),
 		svelte({
 			// enable run-time checks when not in production
 			dev: !production,
