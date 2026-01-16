63901407b5372f9f183fbd510faa373b3fc07229
Eric McCormick <edm00se@gmail.com>
2026-01-16 17:14:47 -0600

    migration to svelte 5

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,21 +1,23 @@
 import App from './App.svelte';
+import { mount } from "svelte";
+
 // import { render, fireEvent } from '@testing-library/svelte';
 
 describe('App Component', () => {
   describe('Default Rendering', () => {
     it('- header', () => {
       const el = document.createElement('div');
-      new App({
-        target: el
-      });
+      mount(App, {
+                target: el
+              });
       const heading = el.querySelector('h1');
       expect(heading.textContent).toBe('edm00se/uses');
     });
     it('- categories', () => {
       const el = document.createElement('div');
-      new App({
-        target: el
-      });
+      mount(App, {
+                target: el
+              });
       const cats = el.querySelectorAll('div.category');
       expect(cats.length>0).toBeTruthy();
     });
