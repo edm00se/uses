1d78177c69db599ed1f1f463660bf3a5e4a1ddd0
Eric McCormick <edm00se@gmail.com>
2026-02-20 20:44:58 -0600

    prettier fix

--- src/routes/page.svelte.spec.ts
+++ src/routes/page.svelte.spec.ts
@@ -4,10 +4,9 @@ import { render } from 'vitest-browser-svelte';
 import Page from './+page.svelte';
 
 describe('/+page.svelte', () => {
-
 	it('- header', async () => {
 		render(Page);
-		
+
 		const heading = page.getByRole('heading', { level: 1 });
 		await expect.element(heading).toBeInTheDocument();
 		expect(heading).toHaveTextContent('edm00se/uses');
